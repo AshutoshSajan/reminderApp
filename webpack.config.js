38964096898fa8f65b6098292c515037d7d12524
AshutoshSajan <44765540+AshutoshSajan@users.noreply.github.com>
2022-10-22 19:03:03 +0530

    code cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
-/* eslint-disable */
 const webpack = require("webpack");
-// const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const HtmlWebpackPlugin = require("html-webpack-plugin");
 
